4a259cca4ba78832a6c5186fd2ed5acc8e055145
Alexander Matskevich <a.matskevich@studionx.ru>
2021-11-02 10:17:07 +0300

    Added exit codes, split text content to files, added read file method.

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -3,6 +3,7 @@
 const fs = require(`fs`).promises;
 const Utils = require(`../../utils`);
 const chalk = require(`chalk`);
+const {ExitCode} = require(`../../constants`);
 
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
@@ -17,93 +18,67 @@ const Dates = {
   MAX: CURRENT_DATE,
 };
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
-
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const FILE_SENTENCES_PATH = `./data/sentences.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
 const createRandomFullText = (sentencesArray) => {
   const text = [];
   for (let i = 0; i < sentencesArray.length; i += 1) {
-    text.push(sentencesArray[Utils.getRandomInt(0, SENTENCES.length - 1)]);
+    text.push(sentencesArray[Utils.getRandomInt(0, sentencesArray.length - 1)]);
   }
 
   return text.join(` `);
 };
 
-const generateOffers = (count) => (
+const readContent = async (filePath) => {
+  const content = await fs.readFile(filePath, `utf8`);
+  return content.trim().split(`\n`);
+};
+
+const generatePublications = (count, textData) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[Utils.getRandomInt(0, TITLES.length - 1)],
-    announce: Utils.shuffle(SENTENCES).slice(0, Utils.getRandomInt(1, 6)).join(` `),
-    fullText: createRandomFullText(SENTENCES),
+    title: textData.titles[Utils.getRandomInt(0, textData.titles.length - 1)],
+    announce: Utils.shuffle(textData.sentences).slice(0, Utils.getRandomInt(1, 6)).join(` `),
+    fullText: createRandomFullText(textData.sentences),
     createdDate: Utils.formatDate(new Date(Utils.getRandomInt(Dates.MIN, Dates.MAX))),
-    category: Utils.shuffle(CATEGORIES).slice(0, Utils.getRandomInt(1, CATEGORIES.length)),
+    category: Utils.shuffle(textData.categories).slice(0, Utils.getRandomInt(1, textData.categories.length)),
   }))
 );
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const textData = {};
+
+    try {
+      textData.sentences = await readContent(FILE_SENTENCES_PATH);
+      textData.titles = await readContent(FILE_TITLES_PATH);
+      textData.categories = await readContent(FILE_CATEGORIES_PATH);
+    } catch (err) {
+      console.error(chalk.red(err));
+      process.exit(ExitCode.FAIL);
+      return;
+    }
+
     const [count] = args;
     const countPublications = Number.parseInt(count, 10) || DEFAULT_COUNT;
 
     if (countPublications > MAX_PUBLICATIONS) {
       console.error(chalk.red(`Не больше ${MAX_PUBLICATIONS} публикаций.`));
+      process.exit(ExitCode.FAIL);
       return;
     }
 
-    const content = JSON.stringify(generateOffers(countPublications));
+    const content = JSON.stringify(generatePublications(countPublications, textData));
 
     try {
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
+      process.exit(ExitCode.SUCCESS);
     } catch (err) {
       console.error(chalk.red(`Can't write data to file...`));
+      process.exit(ExitCode.FAIL);
     }
   }
 };
