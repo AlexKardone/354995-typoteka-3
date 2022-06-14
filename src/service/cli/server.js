2a1ea4f74327cca21059e63ed110e436b3b221fd
Alexander Matskevich <a.matskevich@studionx.ru>
2022-06-14 09:27:29 +0300

    m6t1

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const express = require(`express`);
-const {ExitCode, HttpCode, API_PREFIX} = require(`../../constants`);
+const {HttpCode, API_PREFIX, ExitCode} = require(`../../constants`);
 const {getLogger} = require(`../lib/logger`);
 const routes = require(`../api`);
+const sequelize = require(`../lib/sequelize`);
 
 const DEFAULT_PORT = 3000;
 const logger = getLogger({name: `api`});
@@ -11,13 +12,19 @@ const logger = getLogger({name: `api`});
 module.exports = {
   name: `--server`,
   async run(args) {
+    try {
+      logger.info(`Trying to connect to database...`);
+      await sequelize.authenticate();
+      logger.info(`The connection to database is established`);
+    } catch (err) {
+      logger.error(`An error occurred: ${err.message}`);
+      process.exit(ExitCode.ERROR);
+    }
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
     const app = express();
-
     app.use(express.json());
-
     app.use((req, res, next) => {
       logger.debug(`Request on route ${req.url}`);
       res.on(`finish`, () => {
@@ -25,19 +32,15 @@ module.exports = {
       });
       next();
     });
-
     app.use(API_PREFIX, routes);
-
     app.use((req, res) => {
       res.status(HttpCode.NOT_FOUND)
       .send(`Not found`);
       logger.error(`Route not found: ${req.url}`);
     });
-
     app.use((err, _req, _res, _next) => {
       logger.error(`An error occured on processing request: ${err.message}`);
     });
-
     try {
       app.listen(port, (err) => {
         if (err) {
@@ -47,7 +50,7 @@ module.exports = {
       });
     } catch (err) {
       logger.error(`An error occured: ${err.message}`);
-      process.exit(ExitCode.ERROR);
+      process.exit(1);
     }
   }
 };
