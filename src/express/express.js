c6e885bb598f654d714b838a56f7649c4fb59c83
Alexander Matskevich <a.matskevich@studionx.ru>
2022-06-14 09:42:43 +0300

    m7t1

--- src/express/express.js
+++ src/express/express.js
@@ -1,29 +1,38 @@
 'use strict';
 
 const express = require(`express`);
-const router = require(`./routes/router`);
 const path = require(`path`);
+const mainRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
+const articlesRoutes = require(`./routes/articles-routes`);
 const {HttpCode} = require(`../constants`);
 
-const Dir = {
+const DIR = {
   PUBLIC: `public`,
-  UPLOAD: `upload`,
+  UPLOAD: `upload`
 };
 
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, Dir.PUBLIC)));
-app.use(express.static(path.resolve(__dirname, Dir.UPLOAD)));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(`/`, mainRoutes);
+app.use(`/my`, myRoutes);
+app.use(`/articles`, articlesRoutes);
+
+app.use(express.static(path.resolve(__dirname, DIR.PUBLIC)));
+app.use(express.static(path.resolve(__dirname, DIR.UPLOAD)));
 
 app.use((req, res) => res.status(HttpCode.BAD_REQUEST).render(`errors/404`));
 app.use((err, _req, res, _next) => {
   console.log(err.message);
   res.status(HttpCode.INTERNAL_SERVER_ERROR).render(`errors/500`);
 });
-
-app.use(router);
 
 app.set(`views`, path.resolve(__dirname, `templates`));
 app.set(`view engine`, `pug`);
